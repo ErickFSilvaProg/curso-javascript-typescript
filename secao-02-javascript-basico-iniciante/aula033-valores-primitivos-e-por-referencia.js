55b17fdd853bb02ed9bafcfe42e342d4d0b71eb7
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-07-15 15:36:58 -0300

    JavaScript | TypeScript, 15/07/2025 (tarde)

--- secao-02-javascript-basico-iniciante/aula033-valores-primitivos-e-por-referencia.js
+++ secao-02-javascript-basico-iniciante/aula033-valores-primitivos-e-por-referencia.js
@@ -1,9 +1,10 @@
-/* 
-    - Valores primitivos (imutáveis): string, number, boolean, undefined, null (bigint, symbol).
-    
-    Qualdo os valores são primitivos os valores são literalmente copiados.
-*/
+/**  
+ * - Valores primitivos (imutáveis): string, number, boolean, undefined, null (bigint, symbol). São valores copiados.
+ * 
+ * Quando os valores são primitivos os valores são literalmente copiados.
+ */
 
+/* VARIÁVEIS: */
 let a = 'A';
 let b = a;
 console.log(a, b);
@@ -13,18 +14,55 @@ console.log(a, b);
 console.log();
 
 
-/* 
-    - Valores por referência (mutáveis): array, object, function.
+/** 
+ * - Valores por referência (mutáveis): array, object, function.
+ * 
+ * Quando os valores são passados por referência, os mesmos são apontados na memória e não copiados.
+ */
 
-    Qualdo os valores são por referência os mesmos são referenciados e não copiados.
-*/
+/* ARRAYs: */
+let aa = [1, 2, 3];
+let bb = aa;
+let cc = bb;
 
-let numeros = [1, 2, 3];
-let outros_numeros = numeros;
-console.log(numeros, outros_numeros);
+console.log(aa, bb);
 
-numeros[0] = 10;
-console.log(numeros, outros_numeros);
+aa.push(4);
+console.log(aa, bb);
 
-numeros.push(4);
-console.log(numeros, outros_numeros);
+bb.pop();
+console.log(aa, bb);
+
+aa.push('Luiz');
+console.log(aa, bb, cc);
+console.log();
+
+// Para copiar os valores de 'aa' para 'bb', faça assim:
+bb = [...aa];
+bb.pop();
+console.log();
+
+aa.push(5, 6);
+console.log(aa, bb, cc);
+console.log();
+
+
+/* OBJETOS: */
+let pessoa1 = {
+    nome: 'Erick',
+    sobrenome: 'Ferreira'
+}
+console.log(pessoa1);
+
+let pessoa2 = pessoa1;
+console.log(pessoa2);
+
+pessoa1.nome = 'Gabriel';
+console.log(pessoa2);
+
+// Para copiar os valores da pessoa1 para a pessoa2, faça assim:
+pessoa2 = {...pessoa1};
+console.log(pessoa1, pessoa2);
+
+pessoa1.nome = 'Erick';
+console.log(pessoa1, pessoa2);
