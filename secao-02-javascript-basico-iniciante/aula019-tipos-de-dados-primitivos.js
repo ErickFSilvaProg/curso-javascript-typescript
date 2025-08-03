d520eb09dfca1fc7b0b8128edcb4a95ef2ca662a
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-03 14:35:49 -0300

    JavaScript | TypeScript, 03/08/2025 (tarde)

--- secao-02-javascript-basico-iniciante/aula019-tipos-de-dados-primitivos.js
+++ secao-02-javascript-basico-iniciante/aula019-tipos-de-dados-primitivos.js
@@ -1,34 +1,38 @@
-// Tipos de dados primitivos:
+// Tipos de dados primitivos: String, number, undefined, null, boolean, symbol.
 
 // String:
 const nome = 'Erick';
 const sobrenome = "Ferreira";
-let profissao = `Programador`;
+const profissao = `Programador`;
+console.log(`${nome}: ${typeof nome}`)
+console.log(`${sobrenome}: ${typeof sobrenome}`)
+console.log(`${profissao}: ${typeof profissao}`)
 
 // Number:
 let idade = 38;
-let salario = 6856.45;
+let salario = 5645.34
+console.log(`${idade}: ${typeof idade}`)
+console.log(`${salario}: ${typeof salario}`)
 
-// Underfined: Não aponda para local algum na memória.
+// Underfined: Não aponta para local algum na memória.
 // Utilizada para declarar uma variável sem atibuir valor a mesma.
-let descricaoPessoa;
+// let descricaoPessoa; (undefined)
 
-// Nulo: Não aponda para local algum na memória.
+// Null: Não aponta para local algum na memória.
 // Utilizada para anular intencionalmente uma variável.
 let descricaoCarreira = null;
+console.log(`${descricaoCarreira}: ${typeof descricaoCarreira}`)
 
 // Boolean:
 let ativoEmpresa = true;
 let deficiente = false;
-
-
-// Verificando os tipos:
-console.log(`${nome}: ${typeof nome}`)
-console.log(`${sobrenome}: ${typeof sobrenome}`)
-console.log(`${profissao}: ${typeof profissao}`)
-console.log(`${idade}: ${typeof idade}`)
-console.log(`${salario}: ${typeof salario}`)
-console.log(`${descricaoPessoa}: ${typeof descricaoCarreira}`)
-console.log(`${descricaoCarreira}: ${typeof descricaoCarreira}`)
 console.log(`${ativoEmpresa}: ${typeof ativoEmpresa}`)
 console.log(`${deficiente}: ${typeof deficiente}`)
+
+// Passagem de valor com dados primitivos: os valores são copiados e não referenciados.
+let a = 2;
+let b = a;
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
