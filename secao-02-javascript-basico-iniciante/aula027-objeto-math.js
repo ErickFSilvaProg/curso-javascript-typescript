3e0a64d97a2ee35779bc61c59e69cc371ab2d097
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-09 21:24:12 -0300

    JavaScript | TypeScript, 09/08/2025 (noite)

--- secao-02-javascript-basico-iniciante/aula027-objeto-math.js
+++ secao-02-javascript-basico-iniciante/aula027-objeto-math.js
@@ -1,49 +1,63 @@
 /**
- * - Objeto Math():
-*/
+ * - Objeto Math.
+ */
 
-// Variáveis:
 let num1 = 9.54578;
 let num2 = 2;
-let num_aleatorio = Math.random() * 10;
+let num_aleatorio = Math.random() * (10 - 1) + 1;
 let array1 = [1,2,3,4,5,-10,-50,1500,9,8,7,6];
 
 // Arredondando para baixo:
-console.log('- Arredondando para baixo:')
+console.log("- Arredondando para baixo:");
+console.log(num1);
 console.log(Math.floor(num1));
 console.log();
 
-console.log('- Arredonda para cima:');
+// Arredondando para cima:
+console.log("- Arredondando para cima:");
+console.log(num1);
 console.log(Math.ceil(num1));
 console.log();
 
-console.log('- Arredonda para o mais próximo:');
+// Arredondando para o mais próximo:
+console.log("- Arredondando para o mais próximo:");
+console.log(num1);
 console.log(Math.round(num1));
 console.log();
 
-console.log('- Encontrar o maior valor:');
+// Encontrar o maior valor:
+console.log("- Encontrando o maior valor:");
+console.log(array1);
 console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
 console.log();
 
-console.log('- Encontrar o menor número:');
+// Encontrar o menor número:
+console.log("- Encontrar o menor número:");
+console.log(array1);
 console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
 console.log();
 
-console.log('- Gerar números aleatórios:');
-console.log(Math.random()); // Número aleatórios entre 0 e 1, Sendo que o 1 não é incluído.
-console.log(Math.random().toFixed(1)); // Número aleatório com uma casa decimal.
-console.log(num_aleatorio.toFixed()); // Número aleatório sem casa decimal entre 1 e 10.
+// Gerar números aleatórios:
+console.log("- Gerar números aleatórios:");
+console.log(Math.random()); // Número aleatório entre 0 e 1. O 1 não é incluído.
+console.log(num_aleatorio);
+console.log(Math.round(num_aleatorio)); // Número aleatório entre 1 e 10.
+console.log(Number(num_aleatorio.toFixed())); // Número aleatório entre 1 e 10.
 console.log();
 
-console.log('- Valor de PI:');
+// Valor de PI:
+console.log("- Valor de PI:");
 console.log(Math.PI);
 console.log();
 
-console.log('- Potenciação:');
-console.log(Math.pow(2, 10)); // Potenciação utilizando o método pow().
-console.log(2 ** 10); // Potenciação utilizando o operador.
+// Potenciação:
+console.log("- Potenciação:");
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
 console.log();
 
-console.log('- Raiz quadrada:');
+// Raiz quadrada::
+console.log("- Raiz quadrada:");
 console.log(Math.sqrt(81));
+console.log(81 ** 0.5);
 console.log();
