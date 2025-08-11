2e09de1fe491bf64ebad8514dc22cadb4bad69e3
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-10 22:41:07 -0300

    JavaScript | TypeScript, 10/08/2025 (noite)

--- secao-02-javascript-basico-iniciante/aula029-arrays-basico.js
+++ secao-02-javascript-basico-iniciante/aula029-arrays-basico.js
@@ -1,19 +1,23 @@
 /** 
- * - Array - Básico:
+ * - Array: Básico.
  * 
  * Array é uma lista, uma coleção que armazena dados.
  * Podemos armazenas qualquer tipo de dado no array.
  * É uma boa prática manter um array com o mesmo tipo de dado.
  * O arrays também são indexados como os strings. Mas são indexados pelos elementos.
- * Se o índice do array não existir, ele será criado automaticamente em sua declaração. Se existir o valor será substituído.
- * Na remoção ou na adicão iniial os índices são alterados.
- * Array em JavaScript são objetos.
+ * Se o índice do array não existir, ele será criado automaticamente em sua declaração. Se existir valor ele será substituído.
+ * Na remoção ou na adicão inicial os índices são alterados.
+ * Array, em JavaScript, são objetos indexados.
  * Sempre opite por adicionar ou remover elementos no final do array.
  */
 
+
+// Variáveis:
+let alunoRemovido;
+
 // Criando um array:
-// Posições:       0       1       2
-const alunos = ['Luiz', 'Maria', 'João'];
+// Podição:     0       1        2
+const alunos = ["Luiz", "Maria", "João"];
 console.log(alunos);
 console.log();
 
@@ -23,46 +27,72 @@ console.log(alunos[1]);
 console.log(alunos[2]);
 console.log();
 
-alunos[0] = 'Eduardo';
+// Editando valores do array:
+alunos[0] = "Eduardo";
 console.log(alunos);
 console.log();
 
-alunos[3] = 'Fulano'; // Índice não existe, mas será criado automaticamente.
+// Adicionando um item no array em uma posição que não existe:
+alunos[3] = "Fulano"; // Índice não existe no array. Será criado automaticamente.
 console.log(alunos);
 console.log();
 
-// Adicionar elementos no final do array:
-alunos.push('Luiza');
-console.log(alunos);
+// Verificando o tamanho do array:
+console.log(alunos.length);
 console.log();
 
-// Remover elementos no final do array:
-// O item removido pode ser armazenado em uma variável.
-alunos.pop();
+// Adicionar um item ao array utilizando o 'length':
+alunos[alunos.length] = "Luiza";
+alunos[alunos.length] = "Fábio";
+alunos[alunos.length] = "Luana";
 console.log(alunos);
 console.log();
 
 // Adicionar elementos no início do array:
-alunos.unshift('Erick');
+alunos.unshift("Carlos");
+alunos.unshift("Erick");
 console.log(alunos);
 console.log();
 
-// Remover elementos no início do array:
-// O item removido pode ser armazenado em uma variável.
-alunos.shift()
+// Remover elementos do início do array:
+// Obs.: O item removido pode ser armazenado em uma variável.
+alunoRemovido = alunos.shift();
+console.log(`Aluno removido: ${alunoRemovido}`);
 console.log(alunos);
 console.log();
 
-// Deletar um elemento sem apagar o índice:
+// Adicionando elementos no final do array:
+alunos.push("Otávio");
+alunos.push("Tereza");
+alunos.push("Marcos");
+console.log(alunos);
+console.log();
+
+// Remover elementos do final do array:
+// Obs.: O item removido pode ser armazenado em uma variável.
+alunoRemovido = alunos.pop();
+console.log(`Aluno removido: ${alunoRemovido}`);
+console.log(alunos);
+console.log();
+
+// Apagar um elemento sem remover o índice:
+console.log(alunos);
 delete alunos[1];
 console.log(alunos);
 console.log();
 
-// Listando elementos do array por quantidade (fatiamento):
-console.log(alunos.slice(0, 2)); // A última posição não será exibida.
+// Acessar índices que não existem retornam 'undefined':
+console.log("Ao acessar um índice inexistente o JS retorna:", alunos[50]);
 console.log();
 
-// Verificando que tipo o array é:
+// Listando elementos do array por quantidade (fatiamento):
+// Obs.: A última posição não será exibida.
+console.log(alunos);
+console.log(alunos.slice(0, 6));
+console.log(alunos.slice(0, -4));
+console.log();
+
+// Verificando que tipo é o array:
 console.log(typeof alunos);
-console.log(alunos instanceof Array);
+console.log(alunos instanceof Array ? "Sim, é um array" : "Não é um array");
 console.log();
