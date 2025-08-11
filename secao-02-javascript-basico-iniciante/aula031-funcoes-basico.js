a75fcc499aca3319d2d488b8ba9dbf6d4e0dc672
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-11 16:16:11 -0300

    JavaScript | TypeScript, 11/08/2025 (tarde)

--- secao-02-javascript-basico-iniciante/aula031-funcoes-basico.js
+++ secao-02-javascript-basico-iniciante/aula031-funcoes-basico.js
@@ -4,76 +4,88 @@
  * Funções executam ações.
  * As regras para criar funções são as mesmas das variáveis.
  * Não encerramos o bloco de declaração da função com o ponto e vírgula.
- * Encerramos a declaração de funções anônimas com o ponto e vígula.
+ * Encerramos a declaração de 'funções anônimas' com o ponto e vígula.
  * Tudo que está dentro da função fica protegido por ela.
  * Funções podem receber parâmetros.
- * funções sem retorno apresentam um 'undefined' quando são chamadas.
+ * Funções sem retorno apresentam um 'undefined' quando são chamadas.
  * Tudo o que vier abaixo de 'return' não poderá mais ser executado.
+ * Crie funções únicas com apenas uma especialidades.
  */
 
 
-// Variáveis:
-let saudacao;
-let resultado;
-
-
-// Declarando funções sem retorno:
-function saudacao1(nome) { // Recebendo o parâmetro 'nome'.
+/* Declarando funções sem retorno: */
+function saudacao(nome) {
     console.log(`Bom dia, ${nome}!`);
 }
 
-saudacao1('Erick');
-saudacao1('Maria');
-console.log(saudacao = saudacao1('Felipe')); // Retornará "Felipe" segiudo de um "undefined".
+saudacao("Erick");
+saudacao("Maria");
+saudacao("Felipe");
+console.log();
+
+// Retornará "Bom dia, Felipe!" e em seguida um "underfined".
+let msnSaudacao = saudacao("Luiz");
+console.log(msnSaudacao);
 console.log();
 
 
-// Declarando funções com retorno:
+/* Declarando funções com retorno: */
 function saudacao2(nome) {
-    return `Olá, ${nome}`;
+    return `Bom dia, ${nome}`;
 }
 
-console.log(saudacao2('Pedro Joaquim'));
+let msnSaudacao2 = saudacao2("Pedro");
+console.log(msnSaudacao2);
 console.log();
 
-
 function soma(x, y) {
-    resultado = x + y;
+    const resultado = x + y;
     return resultado;
 }
 
 console.log(soma(2, 2));
 console.log(soma()); // Retornará um NaN.
+console.log(soma("Erick", "Ferreira"));
+console.log();
 
 
-// Declarando funções com valor padrão:
-function soma2(x=1, y=2) {
+/* Declarando funções com valores padrões: */
+function soma2(x = 1, y = 2) {
     let resultado = x + y;
     return resultado;
 }
 
-console.log(soma2(10)); // O valor de 'y' foi omitido. 'Y' passará a valer o valor padrão contido no parâmetro da função.
+// O valor de 'y' foi omitido. 'Y' passará a valer o valor padrão contido no parâmetro da função.
+console.log(soma2(10));
 console.log();
 
 
-// Declarando função anônima:
-const raiz_1 = function (n) {
-    return Math.sqrt(n);
+/* Declarando funções anônimas: */
+const raiz = function (n) {
+    return n ** 0.5;
 };
-console.log(raiz_1(9));
 
-// Declarando função anônima, "arrow function":
-const raiz_2 = (n) => {
+console.log(raiz(9));
+
+
+/* Declarando funções anônimas: Arrow function */
+const raiz2 = (n) => {
     return n ** 0.5;
 }
-console.log(raiz_2(16));
 
-// Declarando função anônima, "arrow function": quando há apenas uma linha dentro do bloco.
-const raiz_3 = (n) => n ** 0.5;
-console.log(raiz_3(25));
+console.log(raiz2(16));
 
 
-// Declarando função anônima, "arrow function": quando há apenas "um parâmetro" e "uma linha dentro do bloco".
-const raiz_4 = n => n ** 0.5;
-console.log(raiz_4(36));
+/* Declarando funções anônimas: Arrow function. */
+// Obs.: Quando há apenas uma linha dentro do bloco.
+const raiz3 = (n) => n ** 0.5;
+
+console.log(raiz3(25));
+
+
+/* Declarando funções anônimas: Arrow function. */
+// Obs.: Quando há apenas um parâmetro e uma linha dentro do bloco.
+const raiz4 = n => n ** 0.5;
+
+console.log(raiz4(36));
 console.log();
