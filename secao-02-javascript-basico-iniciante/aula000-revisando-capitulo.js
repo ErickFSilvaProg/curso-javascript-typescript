13cc501b47cc218f502b0d4ff91300b9df7f20e7
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-12 16:56:58 -0300

    JavaScript | TypeScript, 12/08/2025 (manhã)

---
+++ secao-02-javascript-basico-iniciante/aula000-revisando-capitulo.js
@@ -0,0 +1,11 @@
+/**
+ * Podemos alterar os valores de um array, declarado como 'constante'. Mas não podemos reatribuir novos valores.
+ * 
+ * Quando uma função está dentro de um objeto ela é chamado de método.
+ * Métodos executam ações.
+ * 
+ */
+
+// Array:
+const array = [1, 2, 3];
+console.log(array);
