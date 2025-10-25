e985a5e8aebc7b2c6be1a9c5ae6ba9e3508d34c8
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-10-24 21:50:30 -0300

    JavaScript | TypeScript, 24/10/2025 (noite)

--- secao-02-javascript-basico-iniciante/aula014-constantes-com-const.js
+++ secao-02-javascript-basico-iniciante/aula014-constantes-com-const.js
@@ -6,27 +6,44 @@
  * As constantes não podem conter espaços ou traços.
  * Criamos constantes no formato camelCase.
  * As constantes são case-sensitive.
- * Não podemos alterar o valor de uma constante.
+ * Não podemos modificar o valor de uma constante:
+ * 
+ *      TypeError: Assignment to constant variable.
+ * 
  * Não podemos declarar uma constante sem inicializa-la:
  * 
  *      const nome;
  *      SyntaxError: Missing initializer in const declaration
+ * 
+ * String = texto
+ * Number = número
+ * 
+ * A soma entre um number e um string resulta em uma concatenação entre ambos.
 */
+
+
+const nome = "Erick Ferreira";
+console.log(nome);
+console.log();
 
 const primeiroNumero = 5;
 const segundoNumero = 10;
+const terceiroNumero = "3";
+
 const resultado = primeiroNumero * segundoNumero;
 const resultadoDuplicado = resultado * 2;
 
 let resultadoTriplicado = resultado * 3;
-resultadoTriplicado += "5";
-
-console.log(resultado);
-console.log(resultadoDuplicado);
+resultadoTriplicado = resultadoTriplicado + primeiroNumero;
 console.log(resultadoTriplicado);
 console.log();
 
-console.log(typeof resultado);
-console.log(typeof resultadoDuplicado);
-console.log(typeof resultadoTriplicado);
+
+console.log(typeof primeiroNumero);
+console.log(typeof segundoNumero);
+console.log(typeof terceiroNumero);
+console.log();
+
+console.log(typeof (primeiroNumero + segundoNumero));
+console.log(typeof (primeiroNumero + terceiroNumero));
 console.log();
