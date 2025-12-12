474b9a0e5c8f613e2a3e5aab4daab97fcc04969a
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-12-12 11:52:29 -0300

    JavaScript | TypeScript, 12/12/2025 (manhã)

--- secao-02-javascript-basico-iniciante/aula019-tipos-de-dados-primitivos.js
+++ secao-02-javascript-basico-iniciante/aula019-tipos-de-dados-primitivos.js
@@ -1,49 +1,62 @@
 /**
- * - Tipos de dados primitivos: 
- *      
+ * - Tipos de dados primitivos em javascript:
+ * 
  *      String, number, undefined, null, boolean, symbol.
+ * 
  */
 
-// String:
+
+// - String:
 const nome = 'Erick';
-const sobrenome = "Ferreira";
-const profissao = `Programador`;
-console.log(`${nome}: ${typeof nome}`);
-console.log(`${sobrenome}: ${typeof sobrenome}`);
-console.log(`${profissao}: ${typeof profissao}`);
+const sobrenome = 'Ferreira';
+const profissao = 'Programador';
+
+console.log(`${nome} -> ${typeof nome}`);
+console.log(`${sobrenome} -> ${typeof sobrenome}`);
+console.log(`${profissao} -> ${typeof profissao}`);
 console.log();
 
-// Number:
-let idade = 38;
-let salario = 5645.34;
-console.log(`${idade}: ${typeof idade}`);
-console.log(`${salario}: ${typeof salario}`);
+
+// - Number:
+let idade = 39;
+let salario = 6545.34;
+
+console.log(`${idade} -> ${typeof idade}`);
+console.log(`${salario} -> ${typeof salario}`);
 console.log();
 
-// Underfined: Não aponta para local algum na memória.
-// Utilizada para declarar uma variável sem atibuir valor a mesma.
-let descricaoPessoa; // undefined
-console.log(descricaoPessoa, typeof descricaoPessoa);
+
+// - Undefined: Não aponta para local algum na memória.
+// Utilizada para declarar uma variável sem atribuir valor.
+let nomeSupervisor; // Deixar o seu uso para a linguagem.
+
+console.log(`${nomeSupervisor} -> ${typeof nomeSupervisor}`);
 console.log();
 
-// Null: Não aponta para local algum na memória.
+
+// - Null: Não aponta para local algum na memória.
 // Utilizada para anular intencionalmente uma variável.
-let descricaoCarreira = null;
-console.log(`${descricaoCarreira}: ${typeof descricaoCarreira}`);
+let horasExtras = null;
+
+console.log(`${horasExtras} -> ${typeof horasExtras}`);
 console.log();
 
-// Boolean:
-let ativoEmpresa = true;
+
+// - Boolean:
+let cadastroAtivo = true;
 let deficiente = false;
-console.log(`${ativoEmpresa}: ${typeof ativoEmpresa}`);
-console.log(`${deficiente}: ${typeof deficiente}`);
+
+console.log(`${cadastroAtivo} -> ${typeof cadastroAtivo}`);
+console.log(`${deficiente} -> ${typeof cadastroAtivo}`);
 console.log();
 
-// Passagem de valor com dados primitivos: os valores são copiados e não referenciados.
+
+// - Passagem de valor com dados primitivos: Os valores são copiados e não referenciados.
 let a = 2;
 let b = a;
-console.log(a, b);
+
+console.log(`a = ${a} | b = ${b}`);
 
 a = 3;
-console.log(a, b);
+console.log(`a = ${a} | b = ${b}`);
 console.log();
